0303abbf26e778ac9ee5e9cbc7bb2ee7a2441fca
Yokesh Thirumoorthi <Yokeshthirumoorthi@gmail.com>
2018-05-26 12:33:53 -0700

    add a test case to point 2d

--- src/types.rs
+++ src/types.rs
@@ -1,15 +1,16 @@
 //point2D data type with x and y coordinate values
 //is the basic representation of a point in
 //2d euclidean space
+#[derive(Debug, Eq, PartialEq)]
 pub struct Point2D {
-    x: f64,
-    y: f64,
+    x: u64,
+    y: u64,
 }
 
 //implement methods of point2D datatype
 impl Point2D {
     // a handy method to create new points
-    pub fn new(x: f64, y: f64) -> Point2D {
+    pub fn new(x: u64, y: u64) -> Point2D {
         Point2D { x, y }
     }
 
@@ -17,23 +18,34 @@ impl Point2D {
     //first compare the y-coordinate, if it
     //is same then compare x-coordinate.
     //when both the points are same return any one
-    pub fn pickleft(&self, other: &Point2D) -> &Point2D {
-        if self == other {
-            return &self;
-        }
-        if self.y != other.y {
-            if self.y < other.y {
-                return &self;
-            } else {
-                return &other;
-            }
-        }
-        if self.x != other.x {
-            if self.x < other.x {
-                return &self;
-            } else {
-                return &other;
-            }
-        }
-    }
+    // pub fn pickleft(&self, other: &Point2D) -> &Point2D {
+    //     if self == other {
+    //         return &self;
+    //     }
+    //     if self.y != other.y {
+    //         if self.y < other.y {
+    //             return &self;
+    //         } else {
+    //             return &other;
+    //         }
+    //     }
+    //     if self.x != other.x {
+    //         if self.x < other.x {
+    //             return &self;
+    //         } else {
+    //             return &other;
+    //         }
+    //     }
+    // }
 }
+
+//some test cases for point2D data type.
+#[test]
+fn test_add_new_points() {
+    assert_eq!(Point2D { x: 1, y: 2 }, Point2D::new(1, 2));
+}
+
+//TODO:
+// point2D should accept any integer type. make it generic
+// The generic type should be bound to eq trait
+// add rust doc
