5b49bc3da23fb951131736e32b84bdf76dff9e07
Yokesh Thirumoorthi <Yokeshthirumoorthi@gmail.com>
2018-05-28 21:56:04 -0700

    add comments in graham_scan method

--- src/convexhull.rs
+++ src/convexhull.rs
@@ -11,18 +11,35 @@ use points::Point2D;
 ///exactly the vertices of CH(Q), in counter clockwise
 ///order of their appearance on the boundary
 pub fn graham_scan<'a>(input_set: &Vec<Point2D>) -> Vec<&Point2D> {
+    //find the pivot point in the input set with the
+    //minimum y-coordinate, or the leftmost such point
+    //in case of tie and set the pivot point as first
+    //element of the set
+
+    // input_set.set_pivot();
+
+    //sort the remianing elements in input set by polar
+    //angle in counter clockwise order around pivot point.
+    //(if more than one point has the same angle, remove all
+    //but the one that is farthest from pivot point)
+
+    //input_set.sort_by_polar_angle_ccw();
+
     //panic when input_set has less than or equalto 2 elements
     assert!(input_set.len() > 2);
+
     //initialize the stack that will maintain the candidate points
-    let mut s: Vec<&Point2D> = Vec::new();
-    s.push(&input_set[0]);
-    s.push(&input_set[1]);
-    s.push(&input_set[2]);
+    let mut hull_points: Vec<&Point2D> = Vec::new();
+    hull_points.push(&input_set[0]);
+    hull_points.push(&input_set[1]);
+    hull_points.push(&input_set[2]);
     for i in 3..input_set.len() {
-        while s[s.len() - 2].ccw(&s[s.len() - 1], &input_set[i]) {
-            s.pop();
+        while hull_points[hull_points.len() - 2]
+            .ccw(&hull_points[hull_points.len() - 1], &input_set[i])
+        {
+            hull_points.pop();
         }
-        s.push(&input_set[i])
+        hull_points.push(&input_set[i])
     }
-    s
+    hull_points
 }
