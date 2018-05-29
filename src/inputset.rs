bb74cd6dd238e9273e646ea8d1992d97d7d9f837
Yokesh Thirumoorthi <Yokeshthirumoorthi@gmail.com>
2018-05-29 09:10:41 -0700

    supress warnings

--- src/inputset.rs
+++ src/inputset.rs
@@ -13,7 +13,9 @@ pub fn push(inputset: &mut Vec<Point2D>, point: (f64, f64)) -> &Vec<Point2D> {
 
 ///Finds the point in input set with least y-coordinate or the
 ///left most in case of a match
-fn pick_vertex(input_set: &mut Vec<Point2D>) -> &Vec<Point2D> {
+/// Sets the pivot point of the inputset
+/// as the first element of the input set.
+pub fn set_pivot(input_set: &mut Vec<Point2D>) -> &Vec<Point2D> {
     //panic if there are no elements in the input_set
     assert!(input_set.len() > 0);
     //set the vertex point to be the
@@ -26,24 +28,17 @@ fn pick_vertex(input_set: &mut Vec<Point2D>) -> &Vec<Point2D> {
     input_set
 }
 
-/// Sets the pivot point of the inputset
-/// as the first element of the input set.
-pub fn set_pivot(input_set: &mut Vec<Point2D>) -> Vec<Point2D> {
-    //TODO: implement the functionality
-    Vec::new()
-}
-
 ///Sorts the  elements of input set by polar
 ///angle in counter clockwise order around pivot point.
 ///(if more than one point has the same angle, remove all
 ///but the one that is farthest from pivot point)
-pub fn sort_polar_angle_ccw(input_set: &mut Vec<Point2D>) -> Vec<Point2D> {
+pub fn sort_polar_angle_ccw(_input_set: &mut Vec<Point2D>) -> Vec<Point2D> {
     //TODO: implement the functionality
     Vec::new()
 }
 
 #[test]
-fn test_pick_vertex() {
+fn test_set_pivot() {
     let point_a = Point2D::new(1.0, 2.0);
     let point_b = Point2D::new(1.0, 3.0);
     let point_c = Point2D::new(1.0, 0.0);
@@ -52,5 +47,5 @@ fn test_pick_vertex() {
     let point_c1 = Point2D::new(1.0, 0.0);
     let mut input_set = vec![point_a, point_b, point_c];
     let out_set = vec![point_c1, point_b1, point_a1];
-    assert_eq!(&out_set, pick_vertex(&mut input_set));
+    assert_eq!(&out_set, set_pivot(&mut input_set));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 pub mod points;
 pub mod inputset;
 pub mod convexhull;
