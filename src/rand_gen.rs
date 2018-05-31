753dd34b06bf44ab5a65e33ec84adfd807e24af0
Yokesh Thirumoorthi <Yokeshthirumoorthi@gmail.com>
2018-05-31 14:48:44 -0700

    draw non random circle points

--- src/rand_gen.rs
+++ src/rand_gen.rs
@@ -28,16 +28,25 @@ pub fn generate(number_of_points: u32, between: Range<f64>, shape: Shape) -> Vec
     //     let b = between.ind_sample(&mut range);
     //     output.push(Point2D::new(a, b))
     // }
-    for _ in 0..number_of_points {
-        let t = 2.0 * (f64::consts::PI) * random::<f64>();
-        let u = random::<f64>()+random::<f64>();
-        let r = if u>1.0  { 2.0 - u } else { u };
+    // for _ in 0..number_of_points {
+    //     let t = 2.0 * (f64::consts::PI) * random::<f64>();
+    //     let u = random::<f64>()+random::<f64>();
+    //     let r = if u>1.0  { 2.0 - u } else { u };
         
-        let a = r*(t.cos());
-        let b = r*(t.sin());
+    //     let a = r*(t.cos());
+    //     let b = r*(t.sin());
+    //     output.push(Point2D::new(a, b));
+    // }
+    let center = (0., 0.);
+    let r = 1.0;
+    let mut f: f64 = 0.0;
+    for _ in 0..36 {
+        let a = r*(f.cos());
+        let b = r*(f.sin());
+        f += 2.0 * f64::consts::PI / 36.0;
         output.push(Point2D::new(a, b));
     }
-    
+
     // println!("{:?}", output);
     // draw_plot(&output);
     output
--- tests/rand_gen.rs
+++ tests/rand_gen.rs
@@ -7,5 +7,5 @@ use rand::distributions::{Range};
 
 #[test]
 fn test_generate() {
-    assert_eq!(25, generate(25, Range::new(-1_f64, 1.), Shape::Circle).len());
+    assert_eq!(2, generate(2500, Range::new(-1_f64, 1.), Shape::Circle).len());
 }
