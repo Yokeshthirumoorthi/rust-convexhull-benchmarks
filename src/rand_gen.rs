591cf4b398be45ef43c54f4417a39713ff782d04
Yokesh Thirumoorthi <Yokeshthirumoorthi@gmail.com>
2018-05-31 14:31:20 -0700

    show circle cluster of points

--- src/rand_gen.rs
+++ src/rand_gen.rs
@@ -11,6 +11,10 @@ use points::Point2D;
 
 extern crate rand;
 use rand_gen::rand::distributions::{IndependentSample, Range};
+use rand_gen::rand::random;
+
+use std::f64;
+// use plots::*;
 
 ///Generates n number of random points based on the
 /// shape constraint and returns them as collection of
@@ -19,11 +23,22 @@ pub fn generate(number_of_points: u32, between: Range<f64>, shape: Shape) -> Vec
     let mut range = rand::thread_rng();
     let mut output: Vec<Point2D> = Vec::new();
 
+    // for _ in 0..number_of_points {
+    //     let a = between.ind_sample(&mut range);
+    //     let b = between.ind_sample(&mut range);
+    //     output.push(Point2D::new(a, b))
+    // }
     for _ in 0..number_of_points {
-        let a = between.ind_sample(&mut range);
-        let b = between.ind_sample(&mut range);
-        output.push(Point2D::new(a, b))
+        let t = 2.0 * (f64::consts::PI) * random::<f64>();
+        let u = random::<f64>()+random::<f64>();
+        let r = if u>1.0  { 2.0 - u } else { u };
+        
+        let a = r*(t.cos());
+        let b = r*(t.sin());
+        output.push(Point2D::new(a, b));
     }
     
+    // println!("{:?}", output);
+    // draw_plot(&output);
     output
 }
