8f0f0292751fddca7603d2a76f50b9ad50923b09
Yokesh Thirumoorthi <Yokeshthirumoorthi@gmail.com>
2018-06-02 12:04:48 -0700

    add test triangle

--- tests/convexhull.rs
+++ tests/convexhull.rs
@@ -1,6 +1,7 @@
 extern crate rustalgo;
 use rustalgo::points::*;
 use rustalgo::convexhull::*;
+use rustalgo::inputset_gen::*;
 
 #[test]
 #[should_panic]
@@ -64,4 +65,33 @@ fn test_chans_algorithm() {
     let h3 = Point2D::new(0.0, 0.0);
     let hull_should_be = vec![h1, h2, h3];
     assert_eq!(hull, hull_should_be);
+}
+
+#[test]
+fn test_triangle() {
+    // triangle has 3 vertices
+    let number_of_vertex = 3;
+    
+    let vertex_1 = Point2D::new(-1.0000000000000009, -1.7320508075688767);
+    let vertex_2 = Point2D::new(2.0, 0.0);
+    let vertex_3 = Point2D::new(-0.9999999999999996, 1.7320508075688776);
+
+    let mut input_set_10 = get_input_set(10, number_of_vertex);
+    let mut input_set_100 = get_input_set(100, number_of_vertex);
+    let mut input_set_10000 = get_input_set(10000, number_of_vertex);
+    // let mut input_set_1000000 = get_input_set(1000000, number_of_vertex);
+    // let mut input_set_10000000 = get_input_set(10000000, number_of_vertex);
+
+    assert_eq!(vec![vertex_1, vertex_2, vertex_3], graham_scan(&mut input_set_10));
+    assert_eq!(vec![vertex_1, vertex_2, vertex_3], graham_scan(&mut input_set_100));
+    assert_eq!(vec![vertex_1, vertex_2, vertex_3], graham_scan(&mut input_set_10000));
+    // assert_eq!(vec![vertex_1, vertex_2, vertex_3], graham_scan(&mut input_set_1000000));
+    // assert_eq!(vec![vertex_1, vertex_2, vertex_3], graham_scan(&mut input_set_10000000));
+
+    assert_eq!(vec![vertex_1, vertex_3, vertex_2], jarvis_march(&mut input_set_10));
+    assert_eq!(vec![vertex_1, vertex_3, vertex_2], jarvis_march(&mut input_set_100));
+    assert_eq!(vec![vertex_1, vertex_3, vertex_2], jarvis_march(&mut input_set_10000));
+    // assert_eq!(vec![vertex_1, vertex_3, vertex_2], jarvis_march(&mut input_set_1000000));
+    // assert_eq!(vec![vertex_1, vertex_3, vertex_2], jarvis_march(&mut input_set_10000000));
+    // assert_eq!(hull_should_be, chans_algorithm(&mut input_set_10));
 }
