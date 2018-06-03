9075fc8d6a7a5a6b5efbeebb9830c656dcce7c2c
Yokesh Thirumoorthi <Yokeshthirumoorthi@gmail.com>
2018-06-03 14:57:25 -0700

    execute trangle 100

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,9 @@ use rustalgo::points::*;
 use rustalgo::convexhull::*;
 
 fn main() {
-    generate_sample_to_file(100, 3);
-    let mut triangle_10: Vec<Point2D> = triangle_10().iter().map(|p| Point2D::new(p.0,p.1)).collect();
-    println!("Benchmark For Smaple Data");
-    println!("Shape::Triangle, Input size: {}", 10);
-    benchmark_convex_hull_algorithms(&mut triangle_10);
-    println!("----------------------------------------")
+    // generate_sample_to_file(100, 3);
+    let mut triangle_100: Vec<Point2D> = triangle_100().iter().map(|p| Point2D::new(p.0,p.1)).collect();
+    benchmark_convex_hull_algorithms(&mut triangle_100);
 }
 
 /// Generates sample set of data and saves to a file.
@@ -24,7 +21,7 @@ fn main() {
 /// For now, the output is saved to sample.txt and 
 /// manullay moveed out of the file
 /// to a rs file.
-fn generate_sample_to_file (sample_size: u64, vertex_count: u64) {
+pub fn generate_sample_to_file (sample_size: u64, vertex_count: u64) {
     let file_name = "sample.txt";
     let mut file = File::create(file_name).unwrap();
     let sample_set = get_input_set(sample_size, vertex_count);
@@ -38,6 +35,8 @@ fn generate_sample_to_file (sample_size: u64, vertex_count: u64) {
 /// Benchmarks all the 3 algorithms for same input
 /// The output is printed to the console
 fn benchmark_convex_hull_algorithms(input_set: &mut Vec<Point2D>) {
+    println!("Benchmark For Smaple Data");
+    println!("Shape::Triangle, Input size: {}", input_set.len());
     //graham scan algorithm
     let start = PreciseTime::now();
     graham_scan(input_set);
@@ -53,11 +52,12 @@ fn benchmark_convex_hull_algorithms(input_set: &mut Vec<Point2D>) {
     let time = Time::new(start.to(end));
     println!("jarvis_march: {:?} ms", time.milli_seconds());
     //chans algorithm
-    // let start = PreciseTime::now();
-    // chans_algorithm(input_set);
-    // let end = PreciseTime::now();
-    // let time = Time::new(start.to(end));
-    // println!("chans_algorithm: {:?} s", time.seconds());
+    let start = PreciseTime::now();
+    chans_algorithm(input_set);
+    let end = PreciseTime::now();
+    let time = Time::new(start.to(end));
+    println!("chans_algorithm: {:?} ms", time.milli_seconds());
+    println!("----------------------------------------")
 }
 
 /// Provides the duration in various
